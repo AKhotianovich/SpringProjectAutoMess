682deada55b79011f7297bc1cc30b255c4f549e7
AHotianovich <a.khotianovich@gmail.com>
2023-09-19 01:24:16 +0300

    authorisation

--- src/main/java/by/hotianovich/automess/security/UserDetailsServiceImpl.java
+++ src/main/java/by/hotianovich/automess/security/UserDetailsServiceImpl.java
@@ -5,23 +5,30 @@ import by.hotianovich.automess.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
     UserRepository userRepository;
+    private static final Logger logger = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
     @Override
     @Transactional
     public UserDetailsImpl loadUserByUsername(String username) throws UsernameNotFoundException {
+        logger.warn("User: {}", username);
         User user = userRepository.findByEmail(username)
                 .orElseThrow(()-> new UsernameNotFoundException(
                         "User with email" + username + "not found"
                 ));
+        logger.warn(user.toString());
         return UserDetailsImpl.build(user);
     }
 }
